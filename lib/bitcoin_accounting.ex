212e8502101f4ba0a31434001b1279d482047167
marc@roosoft.com <marc@roosoft.com>
2022-10-18 09:58:13 -0400

    created BitcoinAccounting.get_book_entries

--- lib/bitcoin_accounting.ex
+++ lib/bitcoin_accounting.ex
@@ -1,18 +1,46 @@
 defmodule BitcoinAccounting do
-  @moduledoc """
-  Documentation for `BitcoinAccounting`.
-  """
+  alias BitcoinLib.Key.{PublicKey, PublicKeyHash, Address}
 
-  @doc """
-  Hello world.
+  def get_book_entries(xpub) do
+    xpub
+    |> get_address_range(false, 0..19)
+    |> extract_book_entries()
+  end
 
-  ## Examples
+  defp get_address_range(xpub, change?, range) do
+    change_index = get_change_index(change?)
 
-      iex> BitcoinAccounting.hello()
-      :world
+    range
+    |> Enum.map(fn index ->
+      xpub
+      |> PublicKey.deserialize!()
+      |> PublicKey.derive_child!(change_index)
+      |> PublicKey.derive_child!(index)
+      |> PublicKeyHash.from_public_key()
+      |> Address.from_public_key_hash(:p2pkh, :testnet)
+    end)
+  end
 
-  """
-  def hello do
-    :world
+  defp extract_book_entries(address_range) do
+    address_range
+    |> Enum.map(fn address ->
+      history =
+        ElectrumClient.get_address_history(address)
+        |> Enum.map(fn history_item ->
+          ElectrumClient.get_transaction(history_item.txid)
+        end)
+
+      %{address: address, history: history}
+    end)
+    |> List.first()
+  end
+
+  defp get_change_index(change?) do
+    case change?,
+      do:
+        (
+          true -> 1
+          false -> 0
+        )
   end
 end
