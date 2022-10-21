65b6708143d6ce43d083ce8bf76f164cc25bc4a6
marc@roosoft.com <marc@roosoft.com>
2022-10-21 11:53:45 -0400

    added the UNLICENCE

--- mix.exs
+++ mix.exs
@@ -1,13 +1,17 @@
 defmodule BitcoinAccounting.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :bitcoin_accounting,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      package: package()
     ]
   end
 
@@ -16,6 +20,23 @@ defmodule BitcoinAccounting.MixProject do
     [
       extra_applications: [:logger],
       mod: {BitcoinAccounting.Application, []}
+    ]
+  end
+
+  defp docs do
+    [
+      # The main page in the docs
+      main: "BitcoinAccounting",
+      source_ref: @version,
+      source_url: "https://github.com/roosoft/bitcoin_accounting"
+    ]
+  end
+
+  def package do
+    [
+      maintainers: ["Marc Lacoursière"],
+      licenses: ["UNLICENCE"],
+      links: %{"GitHub" => "https://github.com/roosoft/bitcoin_accounting"}
     ]
   end
 
