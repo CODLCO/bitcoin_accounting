198d3d4a2aab20891d0be2313ecdea525c1f96f0
marc@roosoft.com <marc@roosoft.com>
2022-10-24 08:59:02 -0400

    created BitcoinAccounting.AddressRange, simplifying the BitcoinAccounting module

--- lib/bitcoin_accounting.ex
+++ lib/bitcoin_accounting.ex
@@ -1,54 +1,11 @@
 defmodule BitcoinAccounting do
-  require Logger
-
-  alias BitcoinAccounting.JournalEntries
-  alias BitcoinLib.Key.{PublicKey, Address}
+  alias BitcoinAccounting.{AddressRange, JournalEntries}
 
   @spec get_book_entries(binary()) :: list()
   def get_book_entries(xpub) do
     xpub
-    |> get_address_range(false, 0..19)
+    |> AddressRange.get_address_range(false, 0..19)
     |> extract_book_entries()
-  end
-
-  defp get_address_range(xpub, change?, range) do
-    case PublicKey.deserialize(xpub) do
-      {:ok, public_key, network, :bip32} ->
-        get_bip32_address_range(public_key, network, change?, range)
-
-      {:ok, public_key, network, :bip49} ->
-        get_bip49_address_range(public_key, network, change?, range)
-
-      {:error, message} ->
-        Logger.error(message)
-
-      unknown ->
-        Logger.error(unknown |> inspect)
-    end
-  end
-
-  defp get_bip32_address_range(public_key, network, change?, range) do
-    change_index = get_change_index(change?)
-
-    range
-    |> Enum.map(fn index ->
-      public_key
-      |> PublicKey.derive_child!(change_index)
-      |> PublicKey.derive_child!(index)
-      |> Address.from_public_key(:p2pkh, network)
-    end)
-  end
-
-  defp get_bip49_address_range(public_key, network, change?, range) do
-    change_index = get_change_index(change?)
-
-    range
-    |> Enum.map(fn index ->
-      public_key
-      |> PublicKey.derive_child!(change_index)
-      |> PublicKey.derive_child!(index)
-      |> Address.from_public_key(:p2sh, network)
-    end)
   end
 
   defp extract_book_entries(address_range) do
@@ -63,14 +20,5 @@ defmodule BitcoinAccounting do
 
       %{address: address, history: history}
     end)
-  end
-
-  defp get_change_index(change?) do
-    case change?,
-      do:
-        (
-          true -> 1
-          false -> 0
-        )
   end
 end
---
+++ lib/bitcoin_accounting/address_range.ex
@@ -0,0 +1,54 @@
+defmodule BitcoinAccounting.AddressRange do
+  require Logger
+
+  alias BitcoinLib.Key.{PublicKey, Address}
+
+  def get_address_range(xpub, change?, range) do
+    case PublicKey.deserialize(xpub) do
+      {:ok, public_key, network, :bip32} ->
+        get_bip32_address_range(public_key, network, change?, range)
+
+      {:ok, public_key, network, :bip49} ->
+        get_bip49_address_range(public_key, network, change?, range)
+
+      {:error, message} ->
+        Logger.error(message)
+
+      unknown ->
+        Logger.error(unknown |> inspect)
+    end
+  end
+
+  defp get_bip32_address_range(public_key, network, change?, range) do
+    change_index = get_change_index(change?)
+
+    range
+    |> Enum.map(fn index ->
+      public_key
+      |> PublicKey.derive_child!(change_index)
+      |> PublicKey.derive_child!(index)
+      |> Address.from_public_key(:p2pkh, network)
+    end)
+  end
+
+  defp get_bip49_address_range(public_key, network, change?, range) do
+    change_index = get_change_index(change?)
+
+    range
+    |> Enum.map(fn index ->
+      public_key
+      |> PublicKey.derive_child!(change_index)
+      |> PublicKey.derive_child!(index)
+      |> Address.from_public_key(:p2sh, network)
+    end)
+  end
+
+  defp get_change_index(change?) do
+    case change?,
+      do:
+        (
+          true -> 1
+          false -> 0
+        )
+  end
+end
