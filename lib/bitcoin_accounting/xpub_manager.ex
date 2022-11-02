b21ff5bc54772da561ce41ce0d2e485b9cf364af
marc@roosoft.com <marc@roosoft.com>
2022-11-02 13:05:00 -0400

    check addresses until a threshold number of empty ones have been found

--- lib/bitcoin_accounting/xpub_manager.ex
+++ lib/bitcoin_accounting/xpub_manager.ex
@@ -2,17 +2,34 @@ defmodule BitcoinAccounting.XpubManager do
   alias BitcoinAccounting.XpubManager.AddressRange
   alias BitcoinAccounting.{AddressManager}
 
-  def get_receive_entries(xpub, empty_address_count) do
-    get_entries(xpub, false, empty_address_count)
+  def get_receive_entries(xpub, empty_address_request) do
+    IO.puts("receive")
+    get_entries(xpub, [], false, empty_address_request)
   end
 
-  def get_change_entries(xpub, empty_address_count) do
-    get_entries(xpub, true, empty_address_count)
+  def get_change_entries(xpub, empty_address_request) do
+    IO.puts("change")
+    get_entries(xpub, [], true, empty_address_request)
   end
 
-  defp get_entries(xpub, change?, empty_address_count) do
-    xpub
-    |> AddressRange.get_address_range(change?, 0..(empty_address_count - 1))
-    |> AddressManager.extract_book_entries()
+  defp get_entries(xpub, previous_entries, change?, empty_address_request) do
+    previous_entries_count = Enum.count(previous_entries)
+
+    range = previous_entries_count..(previous_entries_count + empty_address_request - 1)
+
+    new_entries =
+      xpub
+      |> AddressRange.get_address_range(change?, range)
+      |> AddressManager.extract_book_entries()
+
+    entries = previous_entries ++ new_entries
+
+    needs_more = !Enum.all?(new_entries, &Enum.empty?(&1.history))
+
+    if needs_more do
+      get_entries(xpub, entries, change?, empty_address_request)
+    else
+      entries
+    end
   end
 end
