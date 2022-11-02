17cf259d661f2a922bcfe85215618c9051a1822a
marc@roosoft.com <marc@roosoft.com>
2022-11-02 11:54:23 -0400

    retrieve both receive and change entries in one call

--- lib/bitcoin_accounting.ex
+++ lib/bitcoin_accounting.ex
@@ -1,11 +1,26 @@
 defmodule BitcoinAccounting do
   alias BitcoinAccounting.{AddressRange, JournalEntries}
 
+  @get_book_entries_defaults %{empty_address_count: 20}
+
   @spec get_book_entries(binary()) :: list()
-  def get_book_entries(xpub, change_chain? \\ false) do
-    xpub
-    |> AddressRange.get_address_range(change_chain?, 0..19)
-    |> extract_book_entries()
+  def get_book_entries(xpub, opts \\ []) do
+    %{empty_address_count: empty_address_count} = Enum.into(opts, @get_book_entries_defaults)
+
+    receive_entries =
+      xpub
+      |> AddressRange.get_address_range(false, 0..(empty_address_count - 1))
+      |> extract_book_entries()
+
+    change_entries =
+      xpub
+      |> AddressRange.get_address_range(true, 0..(empty_address_count - 1))
+      |> extract_book_entries()
+
+    %{
+      receive: receive_entries,
+      change: change_entries
+    }
   end
 
   def get_address_history(address) do
@@ -16,13 +31,17 @@ defmodule BitcoinAccounting do
     end)
   end
 
-  defp extract_book_entries(address_range) do
+  defp extract_book_entries(address_range) when is_list(address_range) do
     address_range
     |> Enum.map(fn address ->
-      history = get_address_history(address)
-
-      %{address: address, history: history}
+      extract_book_entries(address)
     end)
+  end
+
+  defp extract_book_entries(address) when is_binary(address) do
+    history = get_address_history(address)
+
+    %{address: address, history: history}
   end
 
   defp get_journal_entries(%{txid: transaction_id}, address) do
